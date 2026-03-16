e3c00ed7fb18272e6f71cf76364bf6e5a6d09a27
ahnaftanvir21 <ahanftanvirtasin@gmail.com>
2026-03-16 17:56:20 +0600

    Quantity validation and convertion to zero

--- Lab Performance_MID/JS/Validation_Script.js
+++ Lab Performance_MID/JS/Validation_Script.js
@@ -9,5 +9,10 @@ let Unit = 0;
 function getUnit(){
     Unit = document.getElementById("QuantityPerDay").value;
     console.log(Unit);
+    if(Unit < 0){
+        document.getElementById("Error").innerHTML="Unit Can not be Negative";
+        document.getElementById("Error").style.color="red";
+        document.getElementById("QuantityPerDay").value = "0";
+    }
     return false;
 }
