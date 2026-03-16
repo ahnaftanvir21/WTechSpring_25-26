7aef4e8747432a9bb053b90a7cfea42f0415c371
ahnaftanvir21 <ahanftanvirtasin@gmail.com>
2026-03-16 17:35:08 +0600

    getting number of Unit

--- Lab Performance_MID/JS/Validation_Script.js
+++ Lab Performance_MID/JS/Validation_Script.js
@@ -1,6 +1,13 @@
 console.log("Connected");
+let UnitPrice = 0;
 function getUnitPrice(){
-    let UnitPrice = document.getElementById("UnitPrice").value;
+    UnitPrice = document.getElementById("UnitPrice").value;
     console.log(UnitPrice);
+    return false;
+}
+let Unit = 0;
+function getUnit(){
+    Unit = document.getElementById("QuantityPerDay").value;
+    console.log(Unit);
     return false;
 }
