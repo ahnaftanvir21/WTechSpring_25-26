c666bf222503f3f33b170a076c6f5f93b04fdcb3
ahnaftanvir21 <ahanftanvirtasin@gmail.com>
2026-04-05 00:57:39 +0600

    Finish practice

--- MID_Practice/JS/scripct.js
+++ MID_Practice/JS/scripct.js
@@ -1,16 +1,28 @@
 console.log("Connected");
+var cnt = 0;
 function get_text(){
+    cnt++;
     let s = document.getElementById("Text").value;
     console.log(s);
     let p = s.length;
     console.log(p);
     let st = s.split(" ");
+    let i = 0;
+    let st1 = [];
     for(let x of st){
         if(x == ""){
             continue;
         }
         else{
-            console.log(x);
+            st1[i] = x;
+            console.log(st1[i]);
         }
+        i++;
     }
+    document.getElementById("count").innerHTML= "String Length: "+p;
+    document.getElementById("string").innerHTML= "String Element: "+st1;
+    var st2 = st.reverse().join(" ");
+    console.log(st2);
+    document.getElementById("rstring").innerHTML="Reverse String: " + st2;
+    document.getElementById("Analyze").innerHTML = "Analyze Text "+ cnt; 
 }
