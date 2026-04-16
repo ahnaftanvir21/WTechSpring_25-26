2d0039285dffa45682fa4f06d28b4af0c1678642
ahnaftanvir21 <ahanftanvirtasin@gmail.com>
2026-04-16 23:59:34 +0600

    reg form

--- FINAL/Lab 1 php/View/registration.php
+++ FINAL/Lab 1 php/View/registration.php
@@ -3,8 +3,30 @@ include "../Controller/registrationvalidation.php";
 ?>
 
 <!DOCTYPE html>
-
+<head>
+    <title>Registration Log In Form</title>
+</head>
 <body>
-    
+    <form method= "post" action="">
+        <table>
+            <tr>
+                <td>
+                    <p style = 'color: red'>* Required Field</p>
+                </td>
+            </tr>
+            <tr>
+                    <td> <label for ="UserName"> User Name: </label></td>
+                    <td> <input type ="text" id = "name" name = "name"> <?php echo $name ?></td>
+                    <td> <p style = 'color: red'>*</p> </td>
+                </tr>
+                <tr>
+                    <td> <label for ="password"> Password: </label></td>
+                    <td> <input type = "password" id="pass" name ="password"><?php echo $password ?></td>
+                </tr>
+                <tr>
+                    <td><input type = "submit" id = "submit" name = "submit" </td>
+                </tr>
+        </table>
+    </form>
 </body>
 </html>
