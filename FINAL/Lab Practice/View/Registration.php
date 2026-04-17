ff335e6852bb547d31f1a8e654b2ef69aac71da4
ahnaftanvir21 <ahanftanvirtasin@gmail.com>
2026-04-17 12:18:55 +0600

    till gender

--- FINAL/Lab Practice/View/Registration.php
+++ FINAL/Lab Practice/View/Registration.php
@@ -1,5 +1,5 @@
 <?php
-include = "";
+##include = "";
 ?>
 <!DOCTYPE html>
 <head>
@@ -9,7 +9,48 @@ include = "";
     <form method = "post" action="">
         <table>
             <tr>
-                <td><p style = 'color: red'>* requird field</p></td>
+                <td><p style = 'color: red'>* requird field</label></td>
+            </tr>
+            <tr>
+                <td>
+                    <label>Name: </label>
+                </td>
+                <td>
+                    <input type="text">
+                </td>
+                <td><p style = 'color:red'>*</p></td>
+            </tr>
+            <tr>
+                <td>
+                    <label>Email: </label>
+                </td>
+                <td>
+                    <input type="email">
+                </td>
+                <td><p style = 'color:red'>*</p></td>
+            </tr>
+            <tr>
+                <td>
+                    <label>Address: </label>
+                </td>
+                <td>
+                    <textarea name="Address" id="Address"></textarea>
+                </td>
+                <td><p style = 'color:red'>*</p></td>
+            </tr>
+            <tr>
+                <td>
+                    <label>Gender: </label>
+                </td>
+                <td>
+                    <input type="radio" value="male">
+                    <label> Male</label>
+                </td>
+                <td>
+                    <input type="radio" value="female">
+                    <label> Female</label>
+                </td>
+                <td><p style = 'color:red'>*</p></td>
             </tr>
         </table>
     </form>
