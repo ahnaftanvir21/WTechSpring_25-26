9123bef6c566229a2f8e953767cda9713604291f
ahnaftanvir21 <ahanftanvirtasin@gmail.com>
2026-04-17 12:07:22 +0600

    starting and requird field

---
+++ FINAL/Lab Practice/View/Registration.php
@@ -0,0 +1,17 @@
+<?php
+include = "";
+?>
+<!DOCTYPE html>
+<head>
+    <title>Registration</title>
+</head>
+<body>
+    <form method = "post" action="">
+        <table>
+            <tr>
+                <td><p style = 'color: red'>* requird field</p></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
