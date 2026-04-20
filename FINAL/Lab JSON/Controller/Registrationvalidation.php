026183b5f9d00346392713440e0a578302da915e
ahnaftanvir21 <ahanftanvirtasin@gmail.com>
2026-04-20 13:48:56 +0600

    sesson an cookie all set

--- FINAL/Lab JSON/Controller/Registrationvalidation.php
+++ FINAL/Lab JSON/Controller/Registrationvalidation.php
@@ -6,13 +6,13 @@ $website = "";
 $gender = "";
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     $name = $_POST["name"];
-    echo $name;
+    //echo $name;
     $email = $_POST["email"];
-    echo "Email: " .$email;
+    //echo "Email: " .$email;
     $website = $_POST['website'];
     $gender = $_POST["gender"];
-    echo  $gender;
-    if(!empty($name) && strlen($name) > 5 && filter_var($email, FILTER_VALIDATE_EMAIL) && preg_match('@^(?:http://)?([^/]+)@i', $url, $website); && !empty($gender)){
+    //echo  $gender;
+    if(!empty($name) && strlen($name) > 5 && filter_var($email, FILTER_VALIDATE_EMAIL) && preg_match("/\b((https?:\/\/)?[a-zA-Z0-9-]+\.[a-zA-Z]{2,}(\/[a-zA-Z0-9#]+\/?)*)(\/)?\b/", $website) && !empty($gender)){
         $_SESSION["name"] = $name;
         $_SESSION["email"] = $email;
         $_SESSION["website"] = $website;
@@ -21,11 +21,14 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         setcookie('email',$email,time()+3600,"/");
         setcookie('website',$name,time()+3600,"/");
         setcookie('gender',$gender,time()+3600,"/");
-        echo "login Successfull";
+        echo "login Successfull\n";
     }
     
     if(isset($_SESSION["name"]) || isset($_COOKIE["name"]) && isset($_SESSION["email"]) || isset($_COOKIE["email"]) && isset($_SESSION["website"]) || isset($_COOKIE["website"]) && isset($_SESSION["gender"]) || isset($_COOKIE["gender"])){
         echo "Welcome Back!";
+    }
+    else{
+        echo "Please Try Again!!";
     }
 }
 ?>
