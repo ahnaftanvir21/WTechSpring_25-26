5ea7180031b3e66155214686c6b4280dd281d17b
ahnaftanvir21 <ahnaftanvirtasin@gmail.com>
2026-04-26 11:32:44 +0600

    file setup

---
+++ FINAL/Theory_DatabaseConncection/View/Registration.php
@@ -0,0 +1,63 @@
+<?php
+include "../Controller/Registrationvalidation.php";
+?>
+<!DOCTYPE html>
+<head>
+    <title>Registration</title>
+</head>
+<body>
+    <form method = "post" action="">
+        <table>
+            <tr>
+                <td><p style = 'color: red'>* requird field</label></td>
+            </tr>
+            <tr>
+                <td>
+                    <label>Name: </label>
+                </td>
+                <td>
+                    <input type="text" name="name">
+                </td>
+                <td><p style = 'color:red'>*</p></td>
+            </tr>
+            <tr>
+                <td>
+                    <label>Email: </label>
+                </td>
+                <td>
+                    <input type="email" name = "email">
+                </td>
+                <td><p style = 'color:red'>*</p></td>
+            </tr>
+            <tr>
+                <td>
+                    <label>Website: </label>
+                </td>
+                <td>
+                    <textarea name="website" id="website"></textarea>
+                </td>
+                <td><p style = 'color:red'>*</p></td>
+            </tr>
+            <tr>
+                <td>
+                    <label>Gender: </label>
+                </td>
+                <td>
+                    <input type="radio" value="male" name="gender">
+                    <label> Male</label>
+                </td>
+                <td>
+                    <input type="radio" value="female" name="gender">
+                    <label> Female</label>
+                </td>
+                <td><p style = 'color:red'>*</p></td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="submit">
+                </td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
