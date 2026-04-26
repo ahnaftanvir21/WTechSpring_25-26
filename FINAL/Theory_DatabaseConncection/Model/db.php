8cd37d9eddc7878b998781adf973e9fc84bd28e1
ahnaftanvir21 <ahnaftanvirtasin@gmail.com>
2026-04-26 11:33:20 +0600

    connection function setup

--- FINAL/Theory_DatabaseConncection/Model/db.php
+++ FINAL/Theory_DatabaseConncection/Model/db.php
@@ -1,3 +1,12 @@
 <?php
-    
+    class db{
+        function connection(){
+            $db_host = "localhost";
+            $db_user="root";
+            $dp_password="";
+            $db_name="register";
+
+        }
+        
+    }
 ?>
